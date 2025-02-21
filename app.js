f348da02d1db08b416b612c5e8c42b7e1e639a78
dani <danifromecuador@gmail.com>
2025-02-20 23:11:32 -0500

    Create a GET endpoint

--- app.js
+++ app.js
@@ -1,45 +1,63 @@
 const fs = require('fs');
 // TODO: Require the http module
-// const http = require('http')
-/*
-// TODO: Create a server
-const server = http.createServer((req, res)=>{
-  // TODO: Create a url object with request url and host name
-  const url = new URL(request.url, 'http://${request.headers.host}')
+const http = require('http')
 
+// TODO: Create a server
+const server = http.createServer((req, res) => {
+  // TODO: Create a url object with request url and host name
+  const url = new URL(req.url, `http://${req.headers.host}`)
+  // TODO: Create a switch statement based on pathname of url
+  if (url.pathname === '/') {
+    // TODO: Check if request method is GET
+    if (req.method === 'GET') {
+      // TODO: Get value of 'name' query
+      const name = url.searchParams.get('name')
+      // TODO: Write response header
+      res.writeHead(200, { 'Content-Type': 'text/html' })
+      const text = fs.createReadStream('./index.html')
+      text.pipe(res)
+      return
+    }
+    if (req.method === 'POST') {
+      handlePostResponse(req, res)
+      return
+    }
+  }
+  if (url.pathname) {
+    res.writeHead(404, { 'Content-Type': 'text/html' })
+    const text = fs.createReadStream('./404.html')
+    text.pipe(res)
+    return
+  }
 })
 
-  // TODO: Create a switch statement based on pathname of url
-
-      // TODO: Check if request method is GET
-
-        // TODO: Get value of 'name' query
-
-        // TODO: Write response header
-
-        // TODO: Pipe index.html to response
 
 
-      // TODO: Check if request is POST and if so, run handlePostResponse()
 
-      // TODO: Write response header
 
-      // TODO: Pipe 404.html to response
+// TODO: Pipe index.html to response
+
+
+// TODO: Check if request is POST and if so, run handlePostResponse()
+
+// TODO: Write response header
+
+// TODO: Pipe 404.html to response
 
 
 // TODO: Have server listen at port 4001
-
+server.listen(4001, () => { console.log(`Server is listening at ${server.address().port}`) })
 
 // Function for handling POST responses
-function handlePostResponse(request, response){
+function handlePostResponse(request, response) {
   request.setEncoding('utf8');
-  
+
   // Receive chunks on 'data' event and concatenate to body variable
   let body = '';
   request.on('data', function (chunk) {
     body += chunk;
   });
-  
+
   // When done receiving data, select a random choice for server
   // Compare server choice with player's choice and send an appropriate message back
   request.on('end', function () {
@@ -57,9 +75,9 @@ function handlePostResponse(request, response){
     if (choice === randomChoice) {
       message = tied;
     } else if (
-        (choice === 'rock' && randomChoice === 'paper') ||
-        (choice === 'paper' && randomChoice === 'scissors') ||
-        (choice === 'scissors' && randomChoice === 'rock')
+      (choice === 'rock' && randomChoice === 'paper') ||
+      (choice === 'paper' && randomChoice === 'scissors') ||
+      (choice === 'scissors' && randomChoice === 'rock')
     ) {
       message = defeat;
     } else {
@@ -69,24 +87,3 @@ function handlePostResponse(request, response){
     response.end(`You selected ${choice}. ${message}`);
   });
 }
-  */
-const http = require('http')
-
-const data = [
-  { id: 1, name: "John1" },
-  { id: 2, name: "John2" },
-  { id: 3, name: "John3" }
-]
-
-const server = http.createServer((req, res) => {
-  if (req.url === '/items') handleGetEndpoint(res)
-  else res.write('select /items endpoint')
-  res.end()
-})
-
-const handleGetEndpoint = (res) => {
-  res.setHeader('Content-Type', 'application/json')
-  res.write(JSON.stringify(data))
-}
-
-server.listen(8080)
